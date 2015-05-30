3aea69c10d4b655ba69a29fa075842e452ad4caa
ツ Liverbool <nukboon@gmail.com>
2015-05-30 20:10:20 +0700

    add customer model.

---
+++ Model/Customer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DoS\UserBundle\Model;
+
+use Sylius\Component\User\Model\Customer as BaseCustomer;
+
+class Customer extends BaseCustomer implements CustomerInterface
+{
+
+}
---
+++ Model/CustomerInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DoS\UserBundle\Model;
+
+use Sylius\Component\User\Model\CustomerInterface as BaseCustomerInterface;
+
+interface CustomerInterface extends BaseCustomerInterface
+{
+
+}
