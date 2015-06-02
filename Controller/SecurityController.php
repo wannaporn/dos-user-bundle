983a9d54acb1bdd58a9379baf1b050fdd7b7953b
ツ Liverbool <nukboon@gmail.com>
2015-06-02 15:15:52 +0700

    add already login redirect.

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -3,8 +3,43 @@
 namespace DoS\UserBundle\Controller;
 
 use Sylius\Bundle\UserBundle\Controller\SecurityController as BaseSecurityController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class SecurityController extends BaseSecurityController
 {
+    /**
+     * @var string
+     */
+    protected $alreadyLoginRoute;
 
+    /**
+     * @param Request $request
+     *
+     * @return RedirectResponse|void
+     */
+    protected function checkAlreadyLogin(Request $request)
+    {
+        if ($this->get('dos.user.security')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            if ($this->alreadyLoginRoute) {
+                return $this->redirectToRoute($this->alreadyLoginRoute);
+            }
+
+            return $this->redirect($request->getUriForPath('/'));
+        }
+
+        return;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loginAction(Request $request)
+    {
+        if ($response = $this->checkAlreadyLogin($request)) {
+            return $response;
+        }
+
+        return parent::loginAction($request);
+    }
 }
