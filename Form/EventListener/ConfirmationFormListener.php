a959d2de81f9108e091be0314775a6f6be3201e4
ツ Liverbool <nukboon@gmail.com>
2015-06-15 12:59:57 +0700

    do not redirect to confirmation page when user was enabled.

--- Form/EventListener/ConfirmationFormListener.php
+++ Form/EventListener/ConfirmationFormListener.php
@@ -56,6 +56,10 @@ class ConfirmationFormListener implements EventSubscriberInterface
         /** @var UserInterface $user */
         $user = $customer->getUser();
 
+        if ($user->isEnabled()) {
+            return;
+        }
+
         $confirmation->send($user);
 
         $this->requestStack->getCurrentRequest()->headers->set(
