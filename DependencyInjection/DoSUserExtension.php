61ee1df9b9a0ba2a9f98a39c08a98e69b50ae4f0
ツ Liverbool <nukboon@gmail.com>
2015-06-24 17:42:54 +0700

    ;) sylius-user alive.

--- DependencyInjection/DoSUserExtension.php
+++ DependencyInjection/DoSUserExtension.php
@@ -12,7 +12,7 @@ class DoSUserExtension extends AbstractResourceExtension
      */
     public function load(array $config, ContainerBuilder $container)
     {
-        $this->configure($config, new Configuration(), $container,
+        $config = $this->configure($config, new Configuration(), $container,
             self::CONFIGURE_LOADER |
             self::CONFIGURE_DATABASE |
             self::CONFIGURE_PARAMETERS |
