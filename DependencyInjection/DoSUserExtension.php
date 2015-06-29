50fd5ae5e039a19c68024f6b21a8c4723a9fc032
ツ Liverbool <nukboon@gmail.com>
2015-06-29 20:44:27 +0700

    prepend config to nothing do in config file.

--- DependencyInjection/DoSUserExtension.php
+++ DependencyInjection/DoSUserExtension.php
@@ -4,8 +4,9 @@ namespace DoS\UserBundle\DependencyInjection;
 
 use DoS\ResourceBundle\DependencyInjection\AbstractResourceExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
-class DoSUserExtension extends AbstractResourceExtension
+class DoSUserExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -22,5 +23,30 @@ class DoSUserExtension extends AbstractResourceExtension
 
         $container->setParameter('dos.user.confirmation', $config['confirmation']);
         $container->setParameter('dos.user.confirmation.actived', $config['confirmation']['actived']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $config = $this->processConfiguration(new Configuration(), $container->getExtensionConfig($this->getAlias()));
+
+        $container->prependExtensionConfig('sylius_user', array(
+            'classes' => array(
+                'customer' => array(
+                    'model' => $config['classes']['customer']['model'],
+                ),
+                'user' => array(
+                    'model' => $config['classes']['user']['model'],
+                ),
+                'user_oauth' => array(
+                    'model' => $config['classes']['user_oauth']['model'],
+                ),
+                'group' => array(
+                    'model' => $config['classes']['group']['model'],
+                ),
+            ),
+        ));
     }
 }
