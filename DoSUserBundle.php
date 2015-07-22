c94f46a9e6a58d89f39c63284ddbd358291f78f1
ツ Liverbool <nukboon@gmail.com>
2015-07-22 21:44:44 +0700

    add namespace mapping.

--- DoSUserBundle.php
+++ DoSUserBundle.php
@@ -18,4 +18,18 @@ class DoSUserBundle extends AbstractResourceBundle
         $container->addCompilerPass(new Compiler\SyliusOverridePass());
         $container->addCompilerPass(new Compiler\ConfirmationPass());
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getModelInterfaces()
+    {
+        return array(
+            'DoS\UserBundle\Model\UserInterface' => 'dos.model.user.class',
+            'DoS\UserBundle\Model\CustomerInterface' => 'dos.model.customer.class',
+            'DoS\UserBundle\Model\GroupInterface' => 'dos.model.group.class',
+            'DoS\UserBundle\Model\UserOAuthInterface' => 'dos.model.user_oauth.class',
+            'DoS\UserBundle\Model\OneTimePasswordInterface' => 'dos.model.otp.class',
+        );
+    }
 }
