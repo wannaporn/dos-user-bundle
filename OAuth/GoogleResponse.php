e80bcf911456018609a9d5fceb235971d5d02e2c
ツ Liverbool <nukboon@gmail.com>
2015-09-07 13:03:46 +0700

    google response update.

--- OAuth/GoogleResponse.php
+++ OAuth/GoogleResponse.php
@@ -4,19 +4,4 @@ namespace DoS\UserBundle\OAuth;
 
 class GoogleResponse extends ResourceResponse
 {
-    /**
-     * @return null|string
-     */
-    public function getFirstName()
-    {
-        return $this->getPathValue('given_name');
-    }
-
-    /**
-     * @return null|string
-     */
-    public function getLastName()
-    {
-        return $this->getPathValue('family_name');
-    }
 }
