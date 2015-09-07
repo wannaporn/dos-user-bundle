bc9aa02d53f691d98473753342c21b844733ed71
ツ Liverbool <nukboon@gmail.com>
2015-09-07 10:34:05 +0700

    fb gender

--- OAuth/FacebookResponse.php
+++ OAuth/FacebookResponse.php
@@ -2,15 +2,37 @@
 
 namespace DoS\UserBundle\OAuth;
 
+use Sylius\Component\User\Model\CustomerInterface;
+
 class FacebookResponse extends ResourceResponse
 {
     /**
-     * @return string|null
+     * {@inheritdoc}
      */
     public function getProfilePicture()
     {
         return sprintf('https://graph.facebook.com/v2.3/%s/picture?width=400',
             $this->getPathValue('id')
         );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGender()
+    {
+        $gender = CustomerInterface::UNKNOWN_GENDER;
+
+        switch($this->getPathValue('gender')) {
+            case 'male':
+                $gender = CustomerInterface::MALE_GENDER;
+                break;
+
+            case 'female':
+                $gender = CustomerInterface::FEMALE_GENDER;
+                break;
+        }
+
+        return $gender;
     }
 }
